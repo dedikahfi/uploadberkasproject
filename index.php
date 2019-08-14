c69bebe2b5e4084d7beda0b603ff736d04337590
dedikahfi <dedikahfirohmatullah@gmail.com>
2019-08-14 19:06:40 +0700

    add modal for input data

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
+  <script>
+function getDate(){
+    var today = new Date();
+
+document.getElementById("date").value = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+}
+</script>
   <!--Script CSS-->
   <link type="text/css" href='https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css' rel='stylesheet'>
   <link type="text/css" href='https://cdn.datatables.net/responsive/2.2.1/css/responsive.dataTables.min.css' rel='stylesheet'>
@@ -24,10 +30,45 @@
 </nav>
 <br />
 <br />
-<form class="form-inline" method="POST" action="upload.php" enctype="multipart/form-data">
- <input class="form-control" type="file" name="upload"/>
- <button type="submit" class="btn btn-success form-control" name="submit"><span class="glyphicon glyphicon-upload"></span> Upload</button>
-</form>
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modalAddData">Add Data</button>
+<div id="modalAddData" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Add Data</h4>
+				</div>
+				<!-- body modal -->
+				<div class="modal-body">
+            <form method="POST" action="upload.php" enctype="multipart/form-data" id="inputdata">
+            Tanggal <input class="form-control" type="date" value="<?php echo date('Y-m-d'); ?>" name="tanggal" required/><br>
+            No Surat<input class="form-control" type="text" name="nosurat" placeholder="No Surat" required/><br>
+            Asal Departemen<input class="form-control" type="text" name="asaldepartemen" placeholder="Asal Departemen" required/><br>
+            Departement Tujuan<input class="form-control" type="text" name="departementujuan" placeholder="Departemen Tujuan" required/><br>
+            Perihal<input class="form-control" type="text" name="perihal" placeholder="Perihal" required/><br>
+            Jumlah Lampiran<input class="form-control" type="text" name="jumlahlampiran" placeholder="Jumlah Lampiran" required/><br>
+            Cuplikan Redaksi Surat<input class="form-control" type="text" name="cuplikanredaksisurat" placeholder="Cuplikan Redaksi Surat" required/><br>
+            Jenis Surat<br>
+            <select name="listsurat" form="inputdata">
+            <option value="Memo">Memo</option>
+            <option value="BA">BA</option>
+            <option value="Tembusan">Tembusan</option>
+            </select><br><br>
+            Tanggal Upload<input class="form-control" type="date" value="<?php echo date('Y-m-d'); ?>" name="tanggalupload" required disabled/><br>
+          
+            Upload Berkas<input class="form-control" type="file" name="upload"/><br><br>
+            <button class="btn btn-success" type="submit" class="btn btn-success form-control" name="submit"><span class="glyphicon glyphicon-upload"></span> Upload</button>
+            <button class="btn btn-danger" type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </form>
+				</div>
+				<!-- footer modal -->
+				<div class="modal-footer">
+          <p>*Harap lengkapi data<p>
+				</div>
+    </div>
+  </div>
+</div>
+
 <br /><br />
 <div class="form-group">
 
--- upload.php
+++ upload.php
@@ -4,13 +4,14 @@ require_once 'config.php';
 if(ISSET($_POST['submit'])){
 	if($_FILES['upload']['name'] != "") {
 		$file = $_FILES['upload'];
+		$file_nosurat = $_POST['nosurat'];
 		
 		$file_name = $file['name'];
 		$file_temp = $file['tmp_name'];
 		$name = explode('.', $file_name);
 		$path = "files/".$file_name;
 		
-		$conn->query("INSERT INTO `file` (name, file) VALUES('$name[0]', '$path')") or die(mysqli_error($conn));
+		$conn->query("INSERT INTO `file` (name, file, nosurat) VALUES('$name[0]', '$path', '$file_nosurat')") or die(mysqli_error($conn));
 		
 		move_uploaded_file($file_temp, $path);
 		header("location:index.php");
