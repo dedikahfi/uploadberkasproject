267783919a01888001183bf14acc3668a445babb
dedikahfi <dedikahfirohmatullah@gmail.com>
2019-08-14 20:27:05 +0700

    update sql

--- system.sql
+++ system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 13, 2019 at 11:08 AM
+-- Generation Time: Aug 14, 2019 at 03:26 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -31,7 +31,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `file` (
   `file_id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
-  `file` varchar(500) NOT NULL
+  `file` varchar(500) NOT NULL,
+  `nosurat` varchar(200) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `asaldepartemen` varchar(150) DEFAULT NULL,
+  `departementujuan` varchar(150) DEFAULT NULL,
+  `perihal` varchar(150) DEFAULT NULL,
+  `jumlahlampiran` varchar(150) DEFAULT NULL,
+  `cuplikanredaksisurat` varchar(150) DEFAULT NULL,
+  `jenissurat` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
